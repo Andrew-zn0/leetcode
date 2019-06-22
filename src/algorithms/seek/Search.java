bc8adeaf9b0dcd765d79f7099152880ace724466
Jay <444685553@qq.com>
2019-06-22 14:34:37 +0800

    add-search

---
+++ src/algorithms/seek/Search.java
@@ -0,0 +1,33 @@
+package algorithms.seek;
+
+/**
+ * @author Jay
+ * @description 查找方法
+ * @date Created in 2019/6/22 14:30
+ */
+public class Search {
+    /**
+     * 折半查找
+     * 适用于有序表
+     *
+     * @param s
+     * @param low
+     * @param high
+     * @param key
+     * @return
+     */
+    public int binSearch(int[] s, int low, int high, int key) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (s[mid] == key) {
+                return mid;
+            } else if (s[mid] > key) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+}
