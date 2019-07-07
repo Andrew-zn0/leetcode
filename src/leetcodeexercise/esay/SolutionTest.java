b85a8f17372f48d64cb2173d46ae086e095dc8ee
Andrew-zn0 <444685553@qq.com>
2019-07-07 23:18:44 +0800

    add-20,21,26,27,28

--- src/leetcodeexercise/esay/SolutionTest.java
+++ src/leetcodeexercise/esay/SolutionTest.java
@@ -1,9 +1,10 @@
 package leetcodeexercise.esay;
 
 
-import com.sun.org.apache.regexp.internal.RE;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.ForkJoinPool;
 
 /**
@@ -238,7 +239,333 @@ class Solution14Test {
     }
 }
 
+/**
+ * 20. 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ */
+class Solution20Test {
 
+    public static void main(String[] args) {
+
+        System.out.println(isValid("{}[]"));
+    }
+
+    public static boolean isValid(String s) {
+
+        int a = 0, b = 0, c = 0;
+        List<Character> strings = new ArrayList<>();
+
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        if (s.length() == 0) {
+            return true;
+        }
+        int index = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char x = s.charAt(i);
+            if (x == '(' || x == '{' || x == '[') {
+                strings.add(x);
+                index++;
+            }
+            if (index == 0) {
+                return false;
+            }
+            // 此处判断可以用map代替
+            if (x == ')') {
+                index--;
+                Character ch = strings.get(index);
+                if (ch != '(') {
+                    return false;
+                }
+                strings.remove(index);
+            } else if (x == ']') {
+                index--;
+                Character ch = strings.get(index);
+                if (ch != '[') {
+                    return false;
+                }
+                strings.remove(index);
+            } else if (x == '}') {
+                index--;
+                Character ch = strings.get(index);
+                if (ch != '{') {
+                    return false;
+                }
+                strings.remove(index);
+            }
+        }
+        if (index != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /***
+     * jdk自带栈 Stack
+     * @param s
+     * @return
+     */
+    public boolean isValidStack(String s) {
+        int len = s.length();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == ')') {
+                // 匹配到反字符，如果栈为空，或栈顶字符与字符不匹配就不满足条件
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        // 执行到最后如果栈为空，表示匹配
+        return stack.isEmpty();
+    }
+
+}
+
+/**
+ * 21.合并俩个有序链表
+ * <p>
+ * 将两个有序链表合并为一个新的有序链表并返回。
+ * 新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+class Solution21Test {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        l1.next = l3;
+        l2.next = l4;
+
+        ListNode l = new ListNode(-1);
+        ListNode min = l;
+        min.next = l1;
+        min = min.next;
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        // ListNode listNode = mergeTwoLists1(l1, l2);
+        System.out.println("-----------");
+
+    }
+
+    /**
+     * 非递归
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode l = new ListNode(-1);
+        ListNode min = l;
+        while (l1 != null && l2 != null) {
+            if (l2.val <= l1.val) {
+                min.next = l2;
+                l2 = l2.next;
+            } else {
+                min.next = l1;
+                l1 = l1.next;
+            }
+            min = min.next;
+        }
+        min.next = l1 == null ? l2 : l1;
+        return l.next;
+    }
+
+    /**
+     * 递归
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+/**
+ * 26. 定一个排序数组，你需要在原地删除重复出现的元素，
+ * 使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ */
+class Solution26Test {
+    public static void main(String[] args) {
+        int[] ints = {1, 1, 2};
+
+        int i = removeDuplicates(ints);
+        System.out.println("数组长度：" + i);
+        Arrays.stream(ints).forEach(x -> System.out.println(x));
+    }
+
+    /**
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates(int[] nums) {
+
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
+
+/**
+ * 27. 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ */
+class Solution27Test {
+    public static void main(String[] args) {
+        int[] ints = {1, 1, 2, 4, 4, 6, 10};
+
+        int i = removeElement(ints, 1);
+        System.out.println("数组长度：" + i);
+        Arrays.stream(ints).forEach(x -> System.out.println(x));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+
+        }
+        return j;
+    }
+}
+
+/**
+ * 28. 给定一个 haystack 字符串和一个 needle 字符串，
+ * 在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1
+ */
+class Solution28Test {
+
+    public static void main(String[] args) {
+        long l = System.currentTimeMillis();
+        int i = strStr("mississippi", "pi");
+        System.out.println(i + "\n时间：" + (System.currentTimeMillis() - l));
+    }
+
+    /**
+     * 暴力破解
+     * <p>
+     * 时间 o(m*n)
+     * <p>
+     * kmp 算法简洁
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public static int strStr(String haystack, String needle) {
+        // return haystack.indexOf(needle);
+        if ("".equals(needle)) {
+            return 0;
+        }
+        int j = 0;
+        //        for (int i = 0; i < haystack.length(); i++) {
+        //
+        //            while (needle.charAt(j) == haystack.charAt(i)) {
+        //                j++;
+        //                i++;
+        //                if (j == needle.length()) {
+        //                    return i - j;
+        //                }
+        //                if (i == haystack.length()) {
+        //                    return -1;
+        //                }
+        //
+        //            }
+        //            if (j != 0) {
+        //                i = i - j;
+        //                j = 0;
+        //            }
+        //
+        //        }for (int i = 0; i < haystack.length(); i++) {
+        int i = 0;
+        while (i < haystack.length() && j < needle.length()) {
+            if (needle.charAt(j) == haystack.charAt(i)) {
+                j++;
+                i++;
+            } else {
+                i = i - j + 1;
+                j = 0;
+            }
+            if (j == needle.length()) {
+                return i - j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 字符串
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr1(String haystack, String needle) {
+        if (needle.equals("") || haystack.equals(needle)) {
+            return 0;
+        }
+        int m = haystack.length();
+        int n = needle.length();
+        for (int i = 0; i <= m - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+}
 
 
 
