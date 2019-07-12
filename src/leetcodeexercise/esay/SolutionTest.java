35d67c880b2fc432498155e5af87fd82f378dec6
Jay <444685553@qq.com>
2019-07-12 11:24:04 +0800

    add-70

--- src/leetcodeexercise/esay/SolutionTest.java
+++ src/leetcodeexercise/esay/SolutionTest.java
@@ -1067,9 +1067,91 @@ class Solution69Test {
         while (x * x > a) {
             x = (x + a / x) / 2;
         }
-        
+
         return (int) x;
     }
 }
 
+/**
+ * 70.假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。有多少种不同的方法可以爬到楼顶
+ */
+class Solution70Test {
+
+    public static void main(String[] args) {
+        int i = climbStairs(3);
+        System.out.println("共有" + i + "种方法");
+    }
+
+
+    /**
+     * 暴力法:2
+     * <p>
+     * 递归实现
+     * <p>
+     * 空间复杂度：O(n)，递归树的深度可以达到n 。
+     *
+     * @param n
+     * @return
+     */
+    public static int climbStairs2(int n) {
+
+        return climbStairsTest(0, n);
+    }
+
+    /**
+     * i表示倒数第几层
+     * 每一层表示前边两层相加
+     * i==n 则表示第一层
+     *
+     * @param i
+     * @param n
+     * @return
+     */
+    private static int climbStairsTest(int i, int n) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        return climbStairsTest(i + 1, n) + climbStairsTest(i + 2, n);
+    }
+
+    /**
+     * 动态规划
+     * 这个问题可以被分解为一些包含最优子结构的子问题，
+     * 即它的最优解可以从其子问题的最优解来有效地构建，我们可以使用动态规划来解决这一问题。
+     * <p>
+     * 第 ii 阶可以由以下两种方法得到：
+     * <p>
+     * 在第 (i-1) 阶后向上爬一阶。
+     * <p>
+     * 在第 (i-2)阶后向上爬俩阶。
+     * <p>
+     * 所以到达第 ii 阶的方法总数就是到第 (i-1)(i−1) 阶和第 (i-2)(i−2) 阶的方法数之和。
+     * <p>
+     * 令 dp[i]dp[i] 表示能到达第 ii 阶的方法总数：
+     * <p>
+     * dp[i]=dp[i-1]+dp[i-2]
+     * dp[i]=dp[i−1]+dp[i−2]
+     *
+     * @param n
+     * @return
+     */
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        }
+        return dp[n];
+    }
+}
 
