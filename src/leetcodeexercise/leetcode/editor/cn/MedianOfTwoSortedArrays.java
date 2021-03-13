c96036d6021cb61e895210e0791844737d85aa79
jay <444685553@qq.com>
2021-03-13 22:55:05 +0800

    feat(add): 题目练习 findMedianSortedArrays2

---
+++ src/leetcodeexercise/leetcode/editor/cn/MedianOfTwoSortedArrays.java
@@ -0,0 +1,230 @@
+
+//给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums1 = [1,3], nums2 = [2]
+//输出：2.00000
+//解释：合并数组 = [1,2,3] ，中位数 2
+//
+//
+// 示例 2：
+//
+//
+//输入：nums1 = [1,2], nums2 = [3,4]
+//输出：2.50000
+//解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+//
+//
+// 示例 3：
+//
+//
+//输入：nums1 = [0,0], nums2 = [0,0]
+//输出：0.00000
+//
+//
+// 示例 4：
+//
+//
+//输入：nums1 = [], nums2 = [1]
+//输出：1.00000
+//
+//
+// 示例 5：
+//
+//
+//输入：nums1 = [2], nums2 = []
+//输出：2.00000
+//
+//
+//
+//
+// 提示：
+//
+//
+// nums1.length == m
+// nums2.length == n
+// 0 <= m <= 1000
+// 0 <= n <= 1000
+// 1 <= m + n <= 2000
+// -106 <= nums1[i], nums2[i] <= 106
+//
+//
+//
+//
+// 进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+// Related Topics 数组 二分查找 分治算法
+// 👍 3798 👎 0
+
+package leetcodeexercise.leetcode.editor.cn;
+
+
+public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        Solution solution = new MedianOfTwoSortedArrays().new Solution();
+
+        int[] nums = {1, 2};
+        int[] nums1 = {};
+        System.out.println(solution.findMedianSortedArrays(nums1, nums));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     *
+     */
+    class Solution {
+
+
+        /**
+         * 1. 合并数组
+         * 2. 找到中位数
+         * 时间复杂度： O(m+n)
+         * 空间复杂度： O(m+n)
+         *
+         * @param nums1
+         * @param nums2
+         * @return
+         */
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+
+            int nums[] = new int[nums2.length + nums1.length];
+            int k = 0, i = 0, j = 0;
+
+            if (nums1.length == 0) {
+
+                return getMid(nums2);
+            }
+
+            if (nums2.length == 0) {
+
+                return getMid(nums1);
+            }
+
+
+            while (k < nums1.length + nums2.length) {
+
+
+                while (i < nums1.length) {
+
+
+                    if (j == nums2.length || nums1[i] <= nums2[j]) {
+                        nums[k++] = nums1[i];
+                    } else {
+
+                        break;
+
+                    }
+                    i++;
+
+                }
+
+                while (j < nums2.length) {
+
+
+                    if (i == nums2.length || nums1[i] >= nums2[j]) {
+                        nums[k++] = nums2[j];
+                    } else {
+
+                        break;
+
+                    }
+                    j++;
+                }
+
+            }
+
+
+            return getMid(nums);
+
+
+        }
+    }
+
+    private double getMid(int[] nums) {
+        if (nums.length % 2 == 0) {
+
+            return (nums[nums.length / 2 - 1] + nums[nums.length / 2]) / 2.0;
+
+        } else {
+
+            return nums[nums.length / 2 - 1];
+        }
+
+
+    }
+
+    /**
+     * 直接找中位数
+     * 时间复杂度：O(m+n)
+     * 空间复杂度：O(1)
+     *
+     * @param A
+     * @param B
+     * @return
+     */
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            if (aStart < m && (bStart >= n || A[aStart] < B[bStart])) {
+                right = A[aStart++];
+            } else {
+                right = B[bStart++];
+            }
+        }
+        if ((len & 1) == 0)
+            return (left + right) / 2.0;
+        else
+            return right;
+
+
+    }
+
+    /**
+     * 二分法
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        //将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+    }
+
+    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+        if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0) return nums2[start2 + k - 1];
+
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        }
+        else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        }
+    }
+
+}
+
+
+
+
