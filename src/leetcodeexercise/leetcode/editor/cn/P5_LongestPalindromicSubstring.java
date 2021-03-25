26950b59c60764048351bd5414cb1842c5f94319
Jay <444685553@qq.com>
2021-03-25 12:26:59 +0800

    feat(add-test): P5_LongestPalindromicSubstring （中心扩展法）

--- src/leetcodeexercise/leetcode/editor/cn/P5_LongestPalindromicSubstring.java
+++ src/leetcodeexercise/leetcode/editor/cn/P5_LongestPalindromicSubstring.java
@@ -57,35 +57,93 @@ public class P5_LongestPalindromicSubstring {
 
     //力扣代码
     //leetcode submit region begin(Prohibit modification and deletion)
-	class Solution {
-		/**
-		 * 动态规划
-		 * @param s
-		 * @return
-		 */
-		public String longestPalindrome(String s) {
-			int n = s.length();
-			boolean[][] dp = new boolean[n][n];
-			String ans = "";
-			for (int l = 0; l < n; ++l) {
-				for (int i = 0; i + l < n; ++i) {
-					int j = i + l;
-					if (l == 0) {
-						dp[i][j] = true;
-					} else if (l == 1) {
-						dp[i][j] = (s.charAt(i) == s.charAt(j));
-					} else {
-						dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
-					}
-					if (dp[i][j] && l + 1 > ans.length()) {
-						ans = s.substring(i, i + l + 1);
-					}
-				}
-			}
-			return ans;
-		}
-	}
+    class Solution {
+        /**
+         * 动态规划
+         *
+         * @param s
+         * @return
+         */
+        public String longestPalindrome(String s) {
+            int n = s.length();
+            boolean[][] dp = new boolean[n][n];
+            String ans = "";
+            for (int l = 0; l < n; ++l) {
+                for (int i = 0; i + l < n; ++i) {
+                    int j = i + l;
+                    // 1和0情况判断
+                    if (l == 0) {
+                        dp[i][j] = true;
+                    } else if (l == 1) {
+                        dp[i][j] = (s.charAt(i) == s.charAt(j));
 
-    //leetcode submit region end(Prohibit modification and deletion)
+                        // 动态方程
+                    } else {
+                        dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                    }
+                    if (dp[i][j] && l + 1 > ans.length()) {
+                        ans = s.substring(i, i + l + 1);
+                    }
+                }
+            }
+            return ans;
+        }
+
+        /**
+         * 中心扩展
+         *
+         * @param s
+         * @return
+         */
+        public String longestPalindrome1(String s) {
+
+            if (s == null || s.length() == 0) {
+
+                return "";
+            }
+            int start = 0, end = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                // 奇数情况
+                int len1 = expandAroundCenter(s, i, i);
+
+                // 偶数情况
+                int len2 = expandAroundCenter(s, i, i + 1);
+
+                int len = Math.max(len1, len2);
+
+                // 记录每次回文数起始节点坐标
+				if (len > end - start) {
+					start = i - (len - 1) / 2;
+					end = i + len / 2;
+				}
+
+
+            }
+
+            return s.substring(start, end + 1);
+
+        }
+
+        /**
+         * 找出最长长度回文
+         *
+         * @param s
+         * @param left
+         * @param right
+         * @return
+         */
+        public int expandAroundCenter(String s, int left, int right) {
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                --left;
+                ++right;
+            }
+            return right - left - 1;
+        }
+
+
+    }
+
 
 }
